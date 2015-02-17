f8a5b4b6ec2cb72fb169c62d82aa017fe46dd4ec
Mike <as482jtm45@aol.com>
2015-02-16 21:40:40 -0500

    Version has been increased to 1.1.0.1 for added comments and features.
    Semantic versioning scheme is as follows:
    Architecture Change . Features Added . Bugs Fixed . Comments Added

--- Simulog/Properties/AssemblyInfo.cs
+++ Simulog/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Simulog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Multi Character Private Serve Login For Ashita V2")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mykezero")]
 [assembly: AssemblyProduct("Simulog")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
